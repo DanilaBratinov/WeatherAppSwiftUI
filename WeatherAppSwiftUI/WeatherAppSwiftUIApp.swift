2cb3ef67602b1877c7cbfdc0f8f2a85c24425029
DanilaBratinov <bratinov0704@inbox.ru>
2023-03-17 11:18:27 +0300

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Данила Братинов on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
